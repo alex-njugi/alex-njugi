260c9120850acb70d25c4f262fa20a9f7c5e9815
alex-njugi <alexnjugi11@gmail.com>
2025-07-24 17:00:33 +0300

    added projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,7 +5,7 @@ const projects = [
     title: "Portfolio Website.",
     description: "My personal portfolio built with React and Flask, featuring animations and a contact form.",
     image: "/images/portfolio.png",
-    github: "https://github.com/alex-njugi/alex-njugi",
+    github: "https://github.com/alex-njugi",
     live: "#"
   },
   {
@@ -13,14 +13,35 @@ const projects = [
     description: "A community-focused platform built to support charitable initiatives and donations.",
     image: "/images/power.png",
     github: "https://github.com/alex-njugi/the-power-of-compassion",
-    live: "https://the-power-of-compassion.vercel.app/"
+    live: "https://github.com/alex-njugi"
   },
   {
     title: "Talex Suppliers LTD.",
     description: "A modern business site for Talex Suppliers, focused on car accessories and product presentation.",
     image: "/images/talex.png",
-    github: "https://github.com/alex-njugi/talex-suppliers-ltd",
+    github: "https://github.com/alex-njugi",
     live: "https://talex-suppliers-ltd.vercel.app/"
+  },
+  {
+    title: "REJA BEATS Music Player.",
+    description: "A sleek and immersive music player platform, designed to showcase and stream music seamlessly.",
+    image: "/images/reja.png",
+    github: "https://github.com/alex-njugi",
+    live: "https://reja-beats-music-player.vercel.app/"
+  },
+  {
+    title: "Electro Mart.",
+    description: "A modern e-commerce site for Electro Mart, focused on electronics sales, delivery, and product management.",
+    image: "/images/electro.png",
+    github: "https://github.com/alex-njugi",
+    live: "https://electronics-shop-frontend.onrender.com/"
+  },
+  {
+    title: "Firelands Most Wanted.",
+    description: "A modern web platform for Fireland's Most Wanted, focused on tracking fugitives and enabling public crime reporting.",
+    image: "/images/firelands.png",
+    github: "https://github.com/alex-njugi",
+    live: "https://end-of-phase1-project.vercel.app/"
   },
 ];
 
