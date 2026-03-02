6f11c19db194e65538570f980535872c2673dd84
Alex  Njugi  Karanja <alexnjugi11@gmail.com>
2026-03-02 08:31:14 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,47 +1,26 @@
 import React from "react";
-import { FaLinkedin, FaGithub, FaEnvelope, FaWhatsapp } from "react-icons/fa";
+import { FaInstagram, FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer-links">
-        <a 
-          href="https://wa.me/254792814605" 
-          target="_blank" 
-          rel="noreferrer"
-        >
-          <FaWhatsapp size={23}/>
+        <a href="https://www.instagram.com/njugi_ak/" target="_blank" rel="noreferrer">
+          <FaInstagram />
         </a>
-
-        <a 
-          href="https://www.linkedin.com/in/alex-njugi-04521b367/" 
-          target="_blank" 
-          rel="noreferrer"
-        >
+        <a href="https://www.linkedin.com/in/alex-njugi-04521b367/" target="_blank" rel="noreferrer">
           <FaLinkedin />
         </a>
-
-        <a 
-          href="https://github.com/alex-njugi" 
-          target="_blank" 
-          rel="noreferrer"
-        >
+        <a href="https://github.com/alex-njugi" target="_blank" rel="noreferrer">
           <FaGithub />
         </a>
-
-        <a 
-          href="mailto:alexnjugi11@gmail.com" 
-          target="_blank" 
-          rel="noreferrer"
-        >
+        <a href="mailto:alexnjugi11@gmail.com" target="_blank" rel="noreferrer">
           <FaEnvelope />
         </a>
       </div>
-
       <p className="footer-text">
         © {new Date().getFullYear()} Alex Njugi Karanja. All rights reserved.
       </p>
     </footer>
   );
 }
-
